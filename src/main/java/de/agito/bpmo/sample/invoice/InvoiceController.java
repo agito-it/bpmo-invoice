b06433571909b2b47cf26a9740535f51fb5374aa
Joerg Burmeister <joerg.burmeister@agito-it.de>
2013-06-14 08:09:31 +0200

    change controller impl
    Change-Id: Ifbf9f20503089d357ccd17176f740e16cec8e4b0

--- src/main/java/de/agito/bpmo/sample/invoice/InvoiceController.java
+++ src/main/java/de/agito/bpmo/sample/invoice/InvoiceController.java
@@ -16,6 +16,7 @@ import de.agito.cps.core.bpmo.api.controller.BPMOController;
 import de.agito.cps.core.bpmo.api.controller.IBPMOControllerContext;
 import java.math.BigDecimal;
 // @@end
+import java.math.RoundingMode;
 
 // @@begin head:controller
 /**
@@ -25,7 +26,9 @@ import java.math.BigDecimal;
  */
 // @@end
 @BPMO(id = "Invoice", version = "1.0.0", xml = "de/agito/bpmo/sample/invoice/Invoice.bpmo")
-public class InvoiceController extends BPMOController<InvoiceAccess, InvoiceAction, InvoiceLifecycle, InvoiceLanguage, InvoiceProcessActivity, Invoice> {
+public class InvoiceController
+		extends
+		BPMOController<InvoiceAccess, InvoiceAction, InvoiceLifecycle, InvoiceLanguage, InvoiceProcessActivity, Invoice> {
 
 	public InvoiceController(IBPMOControllerContext context) {
 		super(context);
@@ -48,7 +51,8 @@ public class InvoiceController extends BPMOController<InvoiceAccess, InvoiceActi
 
 		if (taxPostions$NetAmount.getValue() != null && taxPostions$TaxRate.getValue() != null)
 			return taxPostions$NetAmount.getValue().divide(new BigDecimal(100))
-					.multiply(new BigDecimal(taxPostions$TaxRate.getValue().getKey()));
+					.multiply(new BigDecimal(taxPostions$TaxRate.getValue().getKey()))
+					.setScale(2, RoundingMode.HALF_UP);
 		return null;
 		// @@end
 	}
@@ -71,7 +75,7 @@ public class InvoiceController extends BPMOController<InvoiceAccess, InvoiceActi
 		if (taxPostions$NetAmount.getValue() != null && taxPostions$TaxRate.getValue() != null)
 			return taxPostions$NetAmount.getValue().divide(new BigDecimal(100))
 					.multiply(new BigDecimal(taxPostions$TaxRate.getValue().getKey()))
-					.add(taxPostions$NetAmount.getValue());
+					.setScale(2, RoundingMode.HALF_UP).add(taxPostions$NetAmount.getValue());
 		return null;
 
 		// @@end
