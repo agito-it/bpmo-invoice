63e6c9b2d48a6ca950c5699788bb10d5c8ad316d
Joerg Burmeister <joerg.burmeister@agito-it.de>
2013-06-18 16:56:30 +0200

    change message access
    Change-Id: Ic692cf5c971b830e3f9b1899ed913e07f687d039

--- src/main/java/de/agito/bpmo/sample/invoice/Messages.java
+++ src/main/java/de/agito/bpmo/sample/invoice/Messages.java
@@ -4,15 +4,15 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-import de.agito.cps.ui.vaadin.bpmo.context.UIClientContextFactory;
+import de.agito.cps.core.context.ClientContextFactory;
 
 public class Messages {
 	private static final String BUNDLE_NAME = "de.agito.bpmo.sample.invoice.messages"; //$NON-NLS-1$
 
 	public static String getString(String key, Locale locale) {
 		try {
-			ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, locale == null ? UIClientContextFactory
-					.getContext().getApp().getLocale() : locale);
+			ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME,
+					locale == null ? ClientContextFactory.getLocale() : locale);
 			return bundle.getString(key);
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
