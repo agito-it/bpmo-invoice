76cecc4f7d6b9b6ba559bb0a841e7328d7c89e14
Joerg Burmeister <joerg.burmeister@agito-it.de>
2014-10-17 16:32:24 +0200

    refine to V7
    Change-Id: Ie0caaeba4fda9b38d6a9012b1bb93cfb3dc15f14

--- activiti/src/main/java/org/agito/bpmo/sample/invoice/app/InvoiceBPMOApplication.java
+++
@@ -1,9 +0,0 @@
-package org.agito.bpmo.sample.invoice.app;
-
-import de.agito.cps.core.annotations.BPMOApplication;
-import de.agito.cps.core.application.ServletBPMOApplication;
-
-@BPMOApplication(id = "Invoice")
-public class InvoiceBPMOApplication extends ServletBPMOApplication {
-
-}
--- activiti/src/main/java/org/agito/bpmo/sample/invoice/recources/InvoiceTextResource.java
+++
@@ -1,9 +0,0 @@
-package org.agito.bpmo.sample.invoice.recources;
-public enum InvoiceTextResource {
-
-MESSAGE_DATE_MUST_BEFORE_INVOICE_DATE,
-
-CAPTION_ACCOUNTING_INFO,
-
-CAPTION_INVOICE_DETAILS;
-}
